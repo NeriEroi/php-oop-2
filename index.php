9e96da2f185b5c35fd69bf92ad7fa837d6a8f333
Michele <micheleierniero@gmail.com>
2021-10-22 16:44:44 +0200

    html e css

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-
     class Buyer {
         public $name;
         public $surname;
@@ -64,11 +62,126 @@
     // SCARPE DI COLORE BIANCO
     $whitenike = new WhiteShoes('Air', 190);
     $whiteconverse = new WhiteShoes('High Top', 110);
-
-
-    var_dump($buyer1);
-    var_dump($buyer2);
-    var_dump($whitenike);
-    var_dump($whiteconverse);
+    
+    // var_dump($buyer1);
+    // var_dump($buyer2);
+    // var_dump($whitenike);
+    // var_dump($whiteconverse);
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Secondo step con i nostri oggetti</title>
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="buyer_1">
+
+            <h1>
+                Acquirente numero uno:
+            </h1>
+
+            <h2>Nome:
+                <?php echo $buyer1->name ?>
+            </h2>
+            <h2>Cognome: 
+                <?php echo $buyer1->surname ?>
+            </h2>
+            <h2> Età:
+                <?php echo $buyer1->age ?> 
+                anni
+            </h2>
+            <h2> Sconto
+                <?php echo $buyer1->discount ?>%
+            </h2>
+            <h2> Paese
+                <?php echo $buyer1->country ?>
+            </h2>
+
+        </div>
+        
+        <div class="buyer_2">
+
+            <h1>
+                Acquirente numero due:
+            </h1>
+
+            <h2>Nome:
+                <?php echo $buyer2->name ?>
+            </h2>
+            <h2>Cognome: 
+                <?php echo $buyer2->surname ?>
+            </h2>
+            <h2> Età:
+                <?php echo $buyer2->age ?> 
+                anni
+            </h2>
+            <h2> Sconto
+                <?php echo $buyer2->discount ?>%
+            </h2>
+            <h2> Paese
+                <?php echo $buyer2->country ?>
+            </h2>
+
+        </div>
+
+    </div>
+
+    <div class="container_2">
+
+        <div class="nike">
+
+            <h1>
+                Attributi Scarpa Nike:
+            </h1>
+
+            <h2> Colore:
+                <?php echo $whitenike->color ?>
+            </h2>
+
+            <h2> Modello:
+                <?php echo $whitenike->type ?>
+            </h2>
+
+            <h2> Prezzo:
+                <?php echo $whitenike->price ?>$
+            </h2>
+            
+        </div>
+
+        <div class="converse">
+
+            <h1>
+                Attributi Scarpa Converse:
+            </h1>
+
+            <h2> Colore:
+                <?php echo $whiteconverse->color ?>
+            </h2>
+
+            <h2> Modello:
+                <?php echo $whiteconverse->type ?>
+            </h2>
+
+            <h2> Prezzo:
+                <?php echo $whiteconverse->price ?>$
+            </h2>
+            
+        </div>
+
+    </div>
+    
+
+    
+    
+</body>
+</html>
